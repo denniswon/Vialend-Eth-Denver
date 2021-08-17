ee72066529e51abf4a0df9aa124f8c075d236ea0
biangbiang <biangbiang@protonmail.com>
2021-08-17 23:10:55 +0800

    Commit the front-end framework code.

--- swapstream/web/src/App.vue
+++ swapstream/web/src/App.vue
@@ -1,25 +1,125 @@
-<template>  
-  <Home msg="Welcome to SwapStream App"/>
+<template>
+  <div class="super_container">
+    <!-- Header -->
+    <header class="header">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div
+              class="
+                header_content
+                d-flex
+                flex-row
+                align-items-center
+                justify-content-start
+              "
+            >
+              <div class="logo">
+                <a href="#">
+                  <div>SwapStream</div>
+                  <div></div>
+                </a>
+              </div>
+              <nav class="main_nav">
+                <ul
+                  class="
+                    d-flex
+                    flex-row
+                    align-items-center
+                    justify-content-start
+                  "
+                >
+                  <li><a href="blog.html">Docs</a></li>
+                  <li><a href="contact.html">Vote</a></li>
+                </ul>
+              </nav>
+              <div
+                class="
+                  header_extra
+                  d-flex
+                  flex-row
+                  align-items-center
+                  justify-content-start
+                  ml-auto
+                "
+              >
+                <div class="book_button trans_200">
+                  <a href="#">Launch App</a>
+                </div>
+              </div>
+              <div class="hamburger ml-auto">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Menu -->
+
+    <div class="menu">
+      <div
+        class="background_image"
+        style="background-image: url(images/menu.jpg)"
+      ></div>
+      <div
+        class="
+          menu_content
+          d-flex
+          flex-column
+          align-items-center
+          justify-content-center
+        "
+      >
+        <ul class="menu_nav_list text-center">
+          <li><a href="index.html">Docs</a></li>
+          <li><a href="about.html">Vote</a></li>
+        </ul>
+        <div class="menu_review"><a href="#">Launch App</a></div>
+      </div>
+    </div>
+
+    <!-- Home -->
+
+    <div class="home">
+      <div
+        class="parallax_background parallax-window"
+        data-parallax="scroll"
+        data-speed="0.8"
+      ></div>
+      <div class="home_container">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <div class="home_content text-center">
+                <div class="home_title"><h1>Welcome to SwapStream</h1></div>
+                <div class="home_text">It's your best choice</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Home from './components/Home.vue'
+//import HelloWorld from "./components/Home.vue";
 
 export default {
-  name: 'App',
-  components: {
-    Home
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* @import "./assets/styles/bootstrap-4.1.2/bootstrap.min.css";
+@import "./assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css";
+@import "./assets/plugins/OwlCarousel2-2.2.1/owl.carousel.css";
+@import "./assets/plugins/OwlCarousel2-2.2.1/owl.theme.default.css";
+@import "./assets/plugins/OwlCarousel2-2.2.1/animate.css";
+@import "./assets/plugins/colorbox/colorbox.css";
+@import "./assets/styles/main_styles.css";
+@import "./assets/styles/responsive.css"; */
 </style>
--- swapstream/web/src/components/Home.vue
+++ swapstream/web/src/components/Home.vue
@@ -1,94 +1,96 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <button id="btn-wallet" v-on:click="linkWallet()">连接wallet</button>
-    <button @click="getAccount()">获取account</button>
-    <input id="input-account" readonly="readonly" type="text" value="" />
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+          >babel</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+          >eslint</a
+        >
+      </li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+          >Forum</a
+        >
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+          >Community Chat</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+          >Twitter</a
+        >
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      </li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li>
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+          >vue-router</a
+        >
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          target="_blank"
+          rel="noopener"
+          >vue-devtools</a
+        >
+      </li>
+      <li>
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+          >vue-loader</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+          >awesome-vue</a
+        >
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Web3 from "../assets/util/web3.min.js";
-
 export default {
-  name: "Home",
+  name: "HelloWorld",
   props: {
     msg: String,
-  },
-  data() {
-    return {
-      web3Provider: null,
-      web3js: null,
-    };
-  },
-  methods: {
-    linkWallet() {
-      console.log("linking");
-      if (window.ethereum) {
-        this.web3Provider = window.ethereum;
-        // 新版需要请求用户授权
-        try {
-          window.ethereum.enable();
-        } catch (error) {
-          alert("用户取消授权");
-          return;
-        }
-      } else if (Web3.web3) {
-        // MetaMask Legacy dapp browsers...
-        this.web3Provider = Web3.web3.currentProvider;
-        console.log("web3.currentProvider:");
-        console.log(Web3.web3.currentProvider);
-      } else {
-        this.web3Provider = new Web3.providers.HttpProvider(
-          "http://localhost:8545"
-        );
-        console.log("https://http-testnet.hecochain.com");
-      }
-      this.web3js = new Web3(this.web3Provider);
-
-      document.getElementById("btn-wallet").innerText = "重新连接";
-    },
-    getAccount() {
-      if (!this.web3js) {
-        document.getElementById("input-account").value = "请先连接 wallet";
-        return;
-      }
-      this.web3js.eth.getAccounts(function (error, result) {
-        if (!error) {
-          document.getElementById("input-account").value = result;
-        } else {
-          document.getElementById("input-account").value = "获取地址失败";
-        }
-      });
-      console.log("获取链 ID", this.web3js.eth.net.getId());
-      console.log("ChainId获取链 ID", this.web3js.eth.getChainId()),
-        this.web3js.eth.net.getId((err, netID) => {
-          // Main Network: 1   表示主网
-          // Ropsten Test Network: 3  //测试网Ropsten
-          // Kovan Test Network: 42  //测试网Kovan
-          // Rinkeby Test Network: 4  //测试网Rinkeby
-          console.log(netID);
-        });
-      console.log("查询当前区块高度", this.web3js.eth.getBlockNumber());
-      this.web3js.eth.getBlockNumber().then(console.log);
-      console.log(
-        "当前地址余额：",
-        this.web3js.eth
-          .getBalance("0x2F63F0B229749147A9eaF813EF62e5EE2b8Db21f")
-          .then(console.log)
-      );
-      //获取当前metamask账户地址的eth余额
-      this.web3js.eth.getCoinbase((err, coinbase) => {
-        this.web3js.eth.getBalance(coinbase).then(console.log);
-      });
-      //通过hash查询交易
-      console.log(
-        "查询交易",
-        this.web3js.eth.getTransaction(
-          "0x239f038ac2bfe5e456f1480c1ba01564d40719132f78f9244858297251d2fded"
-        )
-      );
-    },
   },
 };
 </script>
